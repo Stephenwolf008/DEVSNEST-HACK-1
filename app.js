77f78d06ea883c90c7a6c8df6387ec4854c1e1e1
adityarawat1337x <rkrawat98766@gmail.com>
2021-06-27 20:04:11 +0530

    starting grids

--- app.js
+++ app.js
@@ -2,48 +2,48 @@ const ball = document.querySelector(".ball");
 const cont = document.querySelector(".container");
 const ship = document.querySelector(".ship");
 
-const grid = document.querySelector(".grid");
+// const grid = document.querySelector(".grid");
 
-const blockWidth = 100;
-const blockHeight = 20;
+// const blockWidth = 100;
+// const blockHeight = 20;
 
-class Block {
-  constructor(xAxis, yAxis) {
-    this.bottomLeft = [xAxis, yAxis];
-    this.bottomRight = [xAxis + blockWidth, yAxis];
-    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
-    this.topLeft = [xAxis, yAxis + blockHeight];
-  }
-}
-const blocks = [
-  new Block(10, 270),
-  new Block(120, 270),
-  new Block(230, 270),
-  new Block(340, 270),
-  new Block(450, 270),
-  new Block(10, 240),
-  new Block(120, 240),
-  new Block(230, 240),
-  new Block(340, 240),
-  new Block(450, 240),
-  new Block(10, 210),
-  new Block(120, 210),
-  new Block(230, 210),
-  new Block(340, 210),
-  new Block(450, 210),
-];
+// class Block {
+//   constructor(xAxis, yAxis) {
+//     this.bottomLeft = [xAxis, yAxis];
+//     this.bottomRight = [xAxis + blockWidth, yAxis];
+//     this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+//     this.topLeft = [xAxis, yAxis + blockHeight];
+//   }
+// }
+// const blocks = [
+//   new Block(10, 270),
+//   new Block(120, 270),
+//   new Block(230, 270),
+//   new Block(340, 270),
+//   new Block(450, 270),
+//   new Block(10, 240),
+//   new Block(120, 240),
+//   new Block(230, 240),
+//   new Block(340, 240),
+//   new Block(450, 240),
+//   new Block(10, 210),
+//   new Block(120, 210),
+//   new Block(230, 210),
+//   new Block(340, 210),
+//   new Block(450, 210),
+// ];
 
-const addBlocks = () => {
-  for (let i = 0; i < 15; i++) {
-    const block = document.createElement("div");
-    block.style.left = blocks[i].bottomLeft[0] + "px";
-    block.style.bottom = blocks[i].bottomLeft[1] + "px";
-    block.classList.add("block");
-    grid.appendChild(block);
-  }
-};
+// const addBlocks = () => {
+//   for (let i = 0; i < 15; i++) {
+//     const block = document.createElement("div");
+//     block.style.left = blocks[i].bottomLeft[0] + "px";
+//     block.style.bottom = blocks[i].bottomLeft[1] + "px";
+//     block.classList.add("block");
+//     grid.appendChild(block);
+//   }
+// };
 
-addBlocks();
+// addBlocks();
 
 const Xend = window.innerWidth;
 const Yend = window.innerHeight;
